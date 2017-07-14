7a411508336f26aa96ef742be46c517b56749ab4
Colin Maher <colinmaher94@gmail.com>
2017-07-14 13:27:00 +0100

    Added tcp listener code and updated server gui

--- PPB Server/Server.xaml.cs
+++ PPB Server/Server.xaml.cs
@@ -7,30 +7,108 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Windows;
+using System.Windows.Media;
 
 namespace PPB_Server
 {
-    public partial class Server : Window
+    //Allows the TcpListener Active property to be accessed
+    public class TcpListenerEx : TcpListener
     {
-        public IPAddress ip = IPAddress.Parse("127.0.0.1");
-        public int port = 2000;
-        public TcpListener server;
+        public TcpListenerEx(IPEndPoint localEP) : base(localEP)
+        {
+        }
+        public TcpListenerEx(IPAddress localaddr, int port) : base(localaddr, port)
+        {
+        }
+
+        public new bool Active
+        {
+            get { return base.Active; }
+        }
+    }
+
+    public partial class Server : Window 
+    {
+        TcpListenerEx server;
+        IPAddress ip = IPAddress.Parse("127.0.0.1");  
+        int port = 2000;
+        bool running = false;
 
         public Server()
         {
             InitializeComponent();
+            //UpdateServerStatus();
+        }
+
+        private void UpdateServerStatus()
+        {
+            this.ServerIPLabel.Content = "IP Address: " + ip;
+            this.ServerPortLabel.Content = "Port: " + port;
+
+            //Checks if TcpListener is listening
+            if (server.Active == true)
+            {
+                running = true;
+            }
+
+            else
+            {
+                running = false;
+            }
+
+            if (running == true)
+            {
+                this.StartServerBtn.IsEnabled = false;
+                this.StopServerBtn.IsEnabled = true;
+
+                this.ServerStatusLabel.Content = "Server Online";
+                this.ServerStatusLabel.Foreground = new SolidColorBrush(Colors.Green);          
+            }     
+
+            else if(running == false)
+            {
+                this.StartServerBtn.IsEnabled = true;
+                this.StopServerBtn.IsEnabled = false;
+
+                this.ServerStatusLabel.Content = "Server Offline";
+                this.ServerStatusLabel.Foreground = new SolidColorBrush(Colors.Red);
+            }
         }
 
         private void StartServer_Click(object sender, RoutedEventArgs e)
         {
-            //server.Start();
-            MessageBox.Show("Server Started");
+            try
+            {
+                server = new TcpListenerEx(ip, port);
+                server.Start();
+ 
+                UpdateServerStatus();                   
+            }
+            catch(SocketException)
+            {
+                MessageBox.Show("TCP Server Error");
+            }     
         }
 
         private void StopServer_Click(object sender, RoutedEventArgs e)
         {
-            //server.Stop();
-            MessageBox.Show("Server Stopped");
+            server.Stop();
+            UpdateServerStatus();
+        }
+
+        private void TestDBConn_Click(object sender, RoutedEventArgs e)
+        { 
+            Login("admin", "password");
+            UpdateServerStatus();
+
+        }
+
+        void Listen()  // Listen to incoming connections.
+        {
+            while (running)
+            {
+                TcpClient tcpClient = server.AcceptTcpClient();
+            }
         }
 
         void Login(string username, string password)
@@ -98,12 +176,12 @@ namespace PPB_Server
                 MessageBox.Show("Connection Failed");
 
                 //Uncomment for mysql error messages
-                /* 
+            
                 if (ex is MySqlException)
                 {
                     MessageBox.Show(ex.Message);
                 }
-                */
+               
             }
         }
     }
